854d8f6c5174f4a24eefd6e90e5b91547c2c3d4a
alejandro <asanchezgiraldo@bauer-media.com.au>
2016-11-03 09:08:46 +1100

    Feature/automation upgrade (#21)
    * updated hooks and package json to use latest chimp version and send historical reports
    
    * changes on the config
    
    * remove html report from npm run once

--- automation/features/support/hooks.js
+++ automation/features/support/hooks.js
@@ -1,39 +1,17 @@
+var automation = require('@bxm/automation').default;
+
 var hooks = function () {
-var world = require('./world');
+    var world = require('./world');
 
-var nconf = require('nconf');
-nconf.argv().env();
-var domainName = nconf.get('APP_KEY');
-function randomValue(){
-    return Date.now();
-}
-    
-    this.After(function (scenario) {
-        var randomId = randomValue();
-        var tags='';
-        if (scenario.getTags().length == '1'){
-            tags = scenario.getTags()[0].getName();
-        } else {
-            for (var i=0; i<scenario.getTags().length;i++){
-                tags = scenario.getTags()[i].getName()+","+tags;
-            }
-            //clean tags
-            tags=tags.substr(0, tags.length-1);
-        }
+    var nconf = require('nconf');
+    nconf.argv().env();
+    var domainName = nconf.get('APP_KEY');
 
-
-        if (scenario.isSuccessful()) {
-            console.log("Nothing to record - Test Passed")
-        } else {
-            console.log("if is not Passed is FAILED");
-            browser.url("http://dev.historical-report.bauer-media.net.au/historical_report/reports/insert.php?id="+randomId+"&name="+scenario.getName()+"&status=Failed&domain="+domainName+"&tags="+tags);
-        }
-        browser.deleteCookie();
-    });
-
-    this.registerHandler('AfterFeatures', function (event, callback) {
-        browser.endAll();
-    });
+        this.After(function (scenario) {
+            console.log('run on domain : ' + domainName);
+            automation.sendReport(scenario, 'agent-i',domainName);
+            browser.deleteCookie();
+        });
 };
 
 module.exports = hooks;
