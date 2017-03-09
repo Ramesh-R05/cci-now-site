38da3e8ad1d6be23478607d258e2afce3d03e51a
Kieran Jones <jones.kieran@hotmail.com>
2017-03-09 14:56:10 +1100

    Added defaultSource to config (#133)
    * Added defaultSource to config
    
    * BXMA-316: Updated package.json

--- src/app/config/site.js
+++ src/app/config/site.js
@@ -1,5 +1,6 @@
 export default {
     dateTimeFormat: 'MMM DD, YYYY h:mma',
+    defaultSource: 'Now To Love',
     host: 'http://dev.now-site.bauer-media.net.au',
     prodDomain: 'www.nowtolove.com.au',
     name: 'Now To Love',
