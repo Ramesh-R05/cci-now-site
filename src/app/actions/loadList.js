1dc82d9d19a880d63f9ab486a12e9b5e8dc300f3
Dean Burge <db@users.noreply.github.com>
2017-06-16 13:02:11 +1000

    test deploy (#267)

--- src/app/actions/loadList.js
+++ src/app/actions/loadList.js
@@ -1,4 +1,5 @@
 export default function loadList(context, payload) {
+    console.log('test deploy');
     return context.getService('list').read(payload).then(
         content => context.dispatch('LOAD_LIST', content),
         error => context.dispatch('LOAD_LIST_FAILED', error)
