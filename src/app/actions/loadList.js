6f4026c4b53c82d5743b7663ce0f7bfea376a3ae
Dean Burge <db@users.noreply.github.com>
2017-06-19 09:57:04 +1000

    revert fallthrough test (#270)
    * server asset revert fallthrough test
    * remove temporary test code changes

--- src/app/actions/loadList.js
+++ src/app/actions/loadList.js
@@ -1,5 +1,4 @@
 export default function loadList(context, payload) {
-    console.log('test deploy');
     return context.getService('list').read(payload).then(
         content => context.dispatch('LOAD_LIST', content),
         error => context.dispatch('LOAD_LIST_FAILED', error)
