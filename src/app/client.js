6e27e9f6bd2b61a6b3a662757f2feb95f76f0f5c
Jones <KiJones@acp.net>
2017-07-11 13:31:24 +1000

    Added redux

--- src/app/client.js
+++ src/app/client.js
@@ -3,19 +3,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createElementWithContext } from 'fluxible-addons-react';
 import app from './app';
+import AppContainer from './containers/app';
 import fluxibleConfigPlugin from 'fluxible-plugin-context-config';
 import batchedUpdatePlugin from 'fluxible-addons-react/batchedUpdatePlugin';
 import adConfig from './config/ads';
+import { Provider } from 'react-redux'
+import configureStore from './stores/configureStore';
+
+const dehydratedState = window.App;
+const reduxStore = configureStore(dehydratedState.reduxStoreState);
 
 window.React = React; // For chrome dev tool support
 
 app.plug(fluxibleConfigPlugin());
 app.plug(batchedUpdatePlugin());
 
-app.rehydrate(window.App, (err, context) => {
+app.rehydrate(dehydratedState, (err, context) => {
     if (err) throw err;
-    const mountNode = document.getElementById('app');
     const userAgent = window.navigator.userAgent;
     adConfig.init(context.getComponentContext().config.site.adTaggingId);
-    ReactDOM.render(createElementWithContext(context, { userAgent }), mountNode, () => {});
+    //const rootElement = ReactDOM.render(createElementWithContext(context, { userAgent }), mountNode, () => {});
+
+    ReactDOM.render(
+        <Provider store={reduxStore}>
+            <AppContainer />
+        </Provider>,
+        document.getElementById('app')
+    );
 });
