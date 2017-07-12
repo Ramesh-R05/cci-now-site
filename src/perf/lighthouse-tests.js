d40f1e04c5b5aa4f03bb3b7b67bf24edf00185b2
alejandro <asanchezgiraldo@bauer-media.com.au>
2017-07-12 14:59:57 +1000

    increase perf score (#326)

--- src/perf/lighthouse-tests.js
+++ src/perf/lighthouse-tests.js
@@ -6,22 +6,22 @@ const testLinks = [
     {
         title: 'article',
         url: 'http://now-site.test.bxm.net.au/fashion/red-carpet/automation-test-article-with-hero-image-3663/',
-        expectedScore: 45
+        expectedScore: 49
     },
     {
         title: 'homepage',
         url: 'http://now-site.test.bxm.net.au/',
-        expectedScore: 45
+        expectedScore: 48
     },
     {
         title: 'section',
         url: 'http://now-site.test.bxm.net.au/fashion/',
-        expectedScore: 48
+        expectedScore: 50
     },
     {
         title: 'gallery',
         url: 'http://now-site.test.bxm.net.au/fashion/red-carpet/automation-test-gallery-13302/',
-        expectedScore: 47
+        expectedScore: 50
     }
 ];
 
