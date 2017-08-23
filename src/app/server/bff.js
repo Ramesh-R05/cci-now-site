2dfd33e9042c752a423eb98d04230a389984be4a
Kevin Heard <kev@kevinheard.com>
2017-08-23 10:25:23 +1000

    add content grouping and custom dimension for ga (#371)

--- src/app/server/bff.js
+++ src/app/server/bff.js
@@ -21,7 +21,7 @@ export default function bff(server) {
         stubServer(server, server.locals.config);
         logger.warn('stubbing does not exercise BFF code');
     } else {
-        server.get('/amp/:section/:subsection/:page', pageModules, section, page, headerMeta, responseBody, amp);
+        server.get('/amp/:section/:subsection/:page', pageModules, section, page, article, gallery, headerMeta, responseBody, amp);
         server.get('/sitemap/:section?', sitemap, error);
         server.get(server.locals.config.services.endpoints.list, list, render, error);
         server.get(
