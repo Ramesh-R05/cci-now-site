4be0512352e35ddc6c0a52a4a99e26b5e9f711fe
Greg <sharpdressedcodes@users.noreply.github.com>
2017-10-12 13:32:54 +1100

    Make amp code generic (#447)

--- automation/features/support/step_definitions/footer_step.js
+++ automation/features/support/step_definitions/footer_step.js
@@ -5,7 +5,7 @@ module.exports = function() {
     this.Given(/^I can see the social logo in the footer$/, function () {
         //Validate the existence of the logo
         var socialLogo = browser.getCssProperty(footer.footerSocialLogo, 'background-image').value;
-        expect(socialLogo).toMatch("/assets/logos/follownowtolove.svg");
+        expect(socialLogo).toMatch("/assets/logos/follow-logo.svg");
     });
 
     this.Given(/^I can see the social icons clickable to open its page in the footer$/, function (dataTable) {
