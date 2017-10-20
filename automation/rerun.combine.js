f5074c7074bfe4fe1f8951155d15c303c63ce171
alejandro <asanchezgiraldo@bauer-media.com.au>
2017-10-20 15:03:41 +1100

    update script to add rerun.txt even if all is successful (#465)

--- automation/rerun.combine.js
+++ automation/rerun.combine.js
@@ -6,24 +6,29 @@ var counter;
 
 
 fs.readdir(testFolder, (err, files) => {
-    files.forEach(file => {
-    const stats = fs.statSync(testFolder + file);
-if (stats.size > 0) {
-    fileList.push(testFolder + file);
-};
-});
-console.log(fileList);
-counter = fileList.length;
-fileList.forEach(file => {
-    counter = counter - 1;
-contents = fs.readFileSync(file, 'utf8');
-if (counter > 0) {
-    contents = contents + '\n';
-}
-fs.appendFile("./@rerun.txt", contents, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-});
-});
+        files.forEach(file => {
+        const stats = fs.statSync(testFolder + file);
+
+            if (stats.size > 0) {
+                fileList.push(testFolder + file);
+            };
+        });
+
+    console.log(fileList);
+    counter = fileList.length;
+
+    fs.createWriteStream("@rerun.txt");
+
+    fileList.forEach(file => {
+            counter = counter - 1;
+        contents = fs.readFileSync(file, 'utf8');
+        if (counter > 0) {
+            contents = contents + '\n';
+        }
+        fs.appendFile("./@rerun.txt", contents, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+        });
+    });
 })
