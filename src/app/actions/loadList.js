3e385d992fa46f5a32c350328806a463f7d23f9b
Dean Burge <db@users.noreply.github.com>
2017-11-08 09:16:29 +1100

    fix load more (#495)

--- src/app/actions/loadList.js
+++ src/app/actions/loadList.js
@@ -3,8 +3,8 @@ import listService from '../services/list';
 export default function loadList(context, payload) {
     return listService.read(payload).then(
         (content) => {
-            if (content instanceof Error) context.dispatch('LOAD_CONTENT_FAILED', content);
-            else context.dispatch('LOAD_CONTENT', { ...content, request: { payload } });
+            if (content instanceof Error) context.dispatch('LOAD_LIST_FAILED', content);
+            else context.dispatch('LOAD_LIST', { ...content, request: { payload } });
         },
         error => context.dispatch('LOAD_LIST_FAILED', error)
     );
