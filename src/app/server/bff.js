4fbee4aa42c7a60bcb8e933a6ad9f9b1e399c783
Dean Burge <db@users.noreply.github.com>
2018-02-21 10:33:14 +1100

    remove comscore (#622)

--- src/app/server/bff.js
+++ src/app/server/bff.js
@@ -19,7 +19,7 @@ import rssInfo from './rss/info';
 import stubServer from '../../automation/test_data/contentApi';
 import logger from '../../logger';
 import assetProxy from './bff/middleware/assetProxy';
-import comScore from './bff/middleware/comScore';
+// import comScore from './bff/middleware/comScore';
 
 export default function bff(server) {
     server.get('/api/asset', assetProxy);
@@ -36,7 +36,7 @@ export default function bff(server) {
         server.get(
             server.locals.config.services.endpoints.page,
             pageModules,
-            comScore,
+            // comScore,
             home,
             listing,
             tag,
