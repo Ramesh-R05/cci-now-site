89de9d015b0f936f69a4461d28d345349f81e828
Greg <sharpdressedcodes@users.noreply.github.com>
2018-02-26 14:33:50 +1100

    Add roymorgan pixels to amp. Update server (#628)

--- src/app/config/features.js
+++ src/app/config/features.js
@@ -20,5 +20,12 @@ export default {
     },
     feedCarousel: {
         enabled: true
+    },
+    royMorgan: {
+        enabled: true,
+        pixels: {
+            '20002302': '0j16fz4d', // To Love pixel
+            '20002304': '30tp4z58'  // Women's sites pixel
+        }
     }
 };
