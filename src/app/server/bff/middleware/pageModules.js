d5929683e96f8a5e7547d734c68fa24811218a8a
Greg <sharpdressedcodes@users.noreply.github.com>
2018-04-16 11:26:13 +1000

    ask cms for section theme (#671)

--- src/app/server/bff/middleware/pageModules.js
+++ src/app/server/bff/middleware/pageModules.js
@@ -7,7 +7,7 @@ export default async function pageModules(req, res, next) {
         req.data = {};
         const section = (req.query && req.query.section) || 'home';
         req.data = await getModules(
-            'headernavigation', 'hamburgernavigation', 'footer', 'mustread', 'promoted', 'hero', `${section}hero`
+            'headernavigation', 'hamburgernavigation', 'footer', 'mustread', 'promoted', 'hero', `${section}hero`, `${section}theme`
         );
     } catch (error) {
         logger.error(error);
