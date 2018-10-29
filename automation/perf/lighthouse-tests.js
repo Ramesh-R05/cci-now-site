16060c2be6d826b8ce69cf011a816244d079c40a
Joe Rattanamaneesilp <jrattanamaneesilp@bauerxcelmedia.com.au>
2018-10-29 11:34:11 +1100

    Updated performance test framework (#835)
    * Updated performance test framework
    
    * automation v7.0.0

---
+++ automation/perf/lighthouse-tests.js
@@ -0,0 +1,28 @@
+const lighthouseTests = require('../node_modules/@bxm/automation/lib/execution/lighthouseTests');
+
+const testLinks = [
+    {
+        title: 'NTL-homepage',
+        url: 'http://now-site.test.bxm.net.au/',
+        expectedScore: 0.12
+    },
+    {
+        title: 'NTL-section',
+        url: 'http://now-site.test.bxm.net.au/fashion/',
+        expectedScore: 0.14
+    },
+    {
+        title: 'NTL-article',
+        url: 'http://now-site.test.bxm.net.au/fashion/red-carpet/automation-test-article-with-hero-image-3663/',
+        expectedScore: 0.11
+    },
+    {
+        title: 'NTL-gallery',
+        url: 'http://now-site.test.bxm.net.au/fashion/red-carpet/automation-test-gallery-13302/',
+        expectedScore: 0.13
+    }
+];
+
+testLinks.forEach((doctypeSetting) => {
+    lighthouseTests(doctypeSetting, 'performance');
+});
