8afac0551500d052c6155c194d2bfc0219b845fe
Joe Rattanamaneesilp <jrattanamaneesilp@bauerxcelmedia.com.au>
2018-11-02 17:01:32 +1100

    Updated the perf scores (#846)

--- automation/perf/lighthouse-tests.js
+++ automation/perf/lighthouse-tests.js
@@ -4,22 +4,22 @@ const testLinks = [
     {
         title: 'NTL-homepage',
         url: 'http://now-site.test.bxm.net.au/',
-        expectedScore: 0.12
+        expectedScore: 0.17
     },
     {
         title: 'NTL-section',
         url: 'http://now-site.test.bxm.net.au/fashion/',
-        expectedScore: 0.14
+        expectedScore: 0.17
     },
     {
         title: 'NTL-article',
         url: 'http://now-site.test.bxm.net.au/fashion/red-carpet/automation-test-article-with-hero-image-3663/',
-        expectedScore: 0.11
+        expectedScore: 0.14
     },
     {
         title: 'NTL-gallery',
         url: 'http://now-site.test.bxm.net.au/fashion/red-carpet/automation-test-gallery-13302/',
-        expectedScore: 0.13
+        expectedScore: 0.15
     }
 ];
 
