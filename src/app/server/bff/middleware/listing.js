585e30f96c56c866e205b3fff055e0cf10744e35
Nick Prodromou <nick.prodromou1@gmail.com>
2019-06-25 21:52:20 +1000

    throw in makeRequest to pass smoketest (#943)

--- src/app/server/bff/middleware/listing.js
+++ src/app/server/bff/middleware/listing.js
@@ -1,5 +1,6 @@
 import has from 'lodash/object/has';
 import makeRequest from '../../makeRequest';
+import logger from '../../../../logger';
 
 export default async function listing(req, res, next) {
     try {
@@ -13,7 +14,13 @@ export default async function listing(req, res, next) {
 
         const listingData = await makeRequest(`${req.app.locals.config.services.remote.entity}/?url=${path}`)
             .then(data => data)
-            .catch(() => ({}));
+            .catch(e => {
+                const error = new Error(`entity not found for path: ${path}`);
+                error.status = 404;
+                logger.error({ msg: 'listing middleware: entity not found', e });
+
+                throw error;
+            });
 
         let entityData = { ...listingData };
         let sectionData = listingData;
@@ -22,7 +29,14 @@ export default async function listing(req, res, next) {
         if (subsection) {
             sectionData = await makeRequest(`${req.app.locals.config.services.remote.entity}/?url=/${section}`)
                 .then(data => data)
-                .catch(() => ({}));
+                .catch(e => {
+                    const error = new Error(`entity not found for section: ${section}`);
+                    error.status = 404;
+
+                    logger.error({ msg: 'listing middleware: entity not found', e });
+
+                    throw error;
+                });
 
             subsectionData = listingData;
             entityData = { ...subsectionData };
