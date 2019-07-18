507327d0ceb3ca95074c85fc500e655b27e649f3
Nick Prodromou <nick.prodromou1@gmail.com>
2019-07-18 15:49:09 +1000

    Fix [NO JIRA] properly implement default timeout using beforeFeatures cucumber hook (#957)

--- automation/features/support/hooks.js
+++ automation/features/support/hooks.js
@@ -1,17 +1,19 @@
 'use strict';
 
 module.exports = function() {
-
-    const timeout = 120 * 2000;
-    this.setDefaultTimeout(timeout);
-    console.log(`Updated default timeout to ${timeout / 1000 / 60 * 1} minutes`);
-
     const nconf = require('nconf');
     nconf.argv().env();
     const domainName = nconf.get('APP_KEY');
 
-    this.After((scenario) => {
-        browser.deleteCookie();
+    this.BeforeFeatures(() => {
+        const timeoutInMs = 240000;
+        const timeoutInMins = timeoutInMs / 1000 / 60;
+
+        this.setDefaultTimeout(timeoutInMs);
+        console.log(`Updated default timeout to ${timeoutInMins} minutes`);
     });
 
+    this.After(scenario => {
+        browser.deleteCookie();
+    });
 };
