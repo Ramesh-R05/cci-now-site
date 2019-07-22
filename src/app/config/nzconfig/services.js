39f934b622c3fa36fc46328fcd921548d3d9dddb
Nick Prodromou <nick.prodromou1@gmail.com>
2019-07-22 15:29:56 +1000

    use gateway service for nz (#965)

--- src/app/config/nzconfig/services.js
+++ src/app/config/nzconfig/services.js
@@ -1,13 +1,13 @@
 export default {
     remote: {
-        entity: 'http://services.sit.bxm.internal/entity/v1/nznow',
-        listings: 'http://services.sit.bxm.internal/listing/v1/nznow',
-        module: 'http://services.sit.bxm.internal/module/v1/nznow',
+        entity: 'https://services.sit.bxm.net.au/entity/v1/nznow',
+        listings: 'https://services.sit.bxm.net.au/listing/v1/nznow',
+        module: 'https://services.sit.bxm.net.au/module/v1/nznow',
         sitemap: 'http://sitemap-service.sit.bxm.net.au/v1/nznow',
-        tag: 'http://services.sit.bxm.internal/tag/v1/nznow',
-        search: 'http://services.sit.bxm.internal/es-search/v1/nznow'
+        tag: 'https://services.sit.bxm.net.au/tag/v1/nznow',
+        search: 'https://services.sit.bxm.net.au/es-search/v1/nznow'
     },
     redirect: {
-        url: 'http://services.sit.bxm.internal/redirect/v1/nznow/301'
+        url: 'https://services.sit.bxm.net.au/redirect/v1/nznow/301'
     }
 };
