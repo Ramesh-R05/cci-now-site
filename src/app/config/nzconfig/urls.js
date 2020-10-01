dc43dc188ddb7b0e712f8a85e730705062bfdda6
Cyrus Chen <kingwinchan@126.com>
2020-10-01 10:34:02 +1000

    [BNC-158] [BNC-159] AMP - Site name change: (#1110)

--- src/app/config/nzconfig/urls.js
+++ src/app/config/nzconfig/urls.js
@@ -2,7 +2,8 @@ export default {
     footerUrls: {
         privacy: 'https://www.aremedia.com.au/privacy/',
         advertise: 'https://www.aremedia.com.au/brands/now-to-love/',
-        terms: 'https://www.aremedia.com.au/terms/'
+        terms: 'https://www.aremedia.com.au/terms/',
+        contactUs: 'https://www.aremedia.com.au/about-us/contact-us/'
     },
     newsletterUrl: 'https://www.nowtolove.co.nz/now-nz-newsletter',
     newsletterSignupInBodyCopy: 'https://cb.sailthru.com/join/5ko/signup-nowtolovenz-articlepage'
